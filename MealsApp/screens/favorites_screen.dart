d95a61a7505e71ec35798f353e423f69caeb7848
Buket Karakaş <buketkarakas8@gmail.com>
2020-09-30 23:45:19 +0300

    Create favorites_screen.dart

---
+++ MealsApp/screens/favorites_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import '../widgets/meal_item.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  FavoritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Container(
+        child: Center(
+          child: Text("You have no favorites yet - start adding some'"),
+        ),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              id: favoriteMeals[index].id,
+              affordability: favoriteMeals[index].affordability,
+              complexity: favoriteMeals[index].complexity,
+              duration: favoriteMeals[index].duration,
+              imageUrl: favoriteMeals[index].imageUrl,
+              title: favoriteMeals[index].title);
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
+  }
+}
